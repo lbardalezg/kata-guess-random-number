e56ed0de423f0838b22383472a75d6315ca14b50
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 22:25:52 -0500

    [green] correct message returned when lower

--- make-guessing-number-game.js
+++ make-guessing-number-game.js
@@ -4,6 +4,7 @@ function makeGuessingNumberGame(randomNumberGenerator) {
     const target = randomNumberGenerator.getNumber()
     function guessNumber(number) {
         if(target < number) return "The number is lower"
+        if(target > number) return "The number is higer"
         return "Correct you won"
     }
     return {
