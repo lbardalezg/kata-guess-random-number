9d55d7429a3b6d085d91afaf64f8c84d78fd2c27
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 22:52:26 -0500

    [green] returns You lose when the third guess is incorrect

--- make-guessing-number-game.js
+++ make-guessing-number-game.js
@@ -2,10 +2,18 @@ module.exports = makeGuessingNumberGame
 
 function makeGuessingNumberGame(randomNumberGenerator) {
     const target = randomNumberGenerator.getNumber()
+    let triesNumber = 0
     function guessNumber(number) {
-        if(target < number) return "The number is lower"
-        if(target > number) return "The number is higer"
-        return "Correct you won"
+        if(triesNumber < 2) {
+            triesNumber++
+            if(target < number) return "The number is lower"
+            if(target > number) return "The number is higer"
+            return "Correct you won"
+        }
+        if(triesNumber === 2) {
+            if(target === number) return "Correct you won"
+            return "You lose"
+        }
     }
     return {
         guessNumber
