4ef4b520e08bec735ad73172731917de70bc7b25
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 22:01:07 -0500

    [red] getNumber doesn't returns 10

--- fixed-random-generator.test.js
+++ fixed-random-generator.test.js
@@ -1,8 +1,14 @@
 const makeFixedRandomNumberGenerator = require('./make-fixed-random-generator')
 
-
 test('should have a function getNumber', () => {
     const fixedRandomNumberGenerator = makeFixedRandomNumberGenerator()
     const result = typeof fixedRandomNumberGenerator.getNumber
     expect(result).toBe('function')
+})
+
+test('getNumber function should return the input number', () => {
+    const inputNumber = 10
+    const fixedRandomNumberGenerator = makeFixedRandomNumberGenerator(inputNumber)
+    const result = fixedRandomNumberGenerator.getNumber()
+    expect(result).toBe(inputNumber)
 })
