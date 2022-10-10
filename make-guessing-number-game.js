ec4f044d8a22b49ff5b0fe4febe605d5fe3e24d1
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 22:22:00 -0500

    [green] correct response when higer

--- make-guessing-number-game.js
+++ make-guessing-number-game.js
@@ -1,7 +1,9 @@
 module.exports = makeGuessingNumberGame
 
 function makeGuessingNumberGame(randomNumberGenerator) {
+    const target = randomNumberGenerator.getNumber()
     function guessNumber(number) {
+        if(target < number) return "The number is lower"
         return "Correct you won"
     }
     return {
