ef8bdd0c66a88aa7e68df6b8d0875e6eda907686
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 22:17:03 -0500

    [refactor] Refactored to group user cases

--- guessign-number-game.spec.js
+++ guessign-number-game.spec.js
@@ -1,10 +1,12 @@
 const makeFixedRandomNumberGenerator = require('./make-fixed-random-generator')
 const makeGuessingNumberGame = require('./make-guessing-number-game')
 
-test('User case 1', () => {
-    const randomNumberGenerator = makeFixedRandomNumberGenerator(5)
-    const guessingNumberGame = makeGuessingNumberGame(randomNumberGenerator)
-    const firstGuessResult = guessingNumberGame.guessNumber(5)
-    const successMessage = "Correct you won"
-    expect(firstGuessResult).toBe(successMessage)
+describe('User case 1: wins on the first move', () => {
+    test('first move', () => {
+        const randomNumberGenerator = makeFixedRandomNumberGenerator(5)
+        const guessingNumberGame = makeGuessingNumberGame(randomNumberGenerator)
+        const firstGuessResult = guessingNumberGame.guessNumber(5)
+        const successMessage = "Correct you won"
+        expect(firstGuessResult).toBe(successMessage)
+    })
 })
