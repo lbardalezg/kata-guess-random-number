f0c3b63a8a91eb533b3c2180e2c506c801bc67bd
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 23:05:30 -0500

    refactored

--- make-guessing-number-game.js
+++ make-guessing-number-game.js
@@ -4,16 +4,17 @@ function makeGuessingNumberGame(randomNumberGenerator) {
     const target = randomNumberGenerator.getNumber()
     let triesNumber = 0
     function guessNumber(number) {
-        if(triesNumber < 2) {
-            triesNumber++
-            if(target < number) return "The number is lower"
-            if(target > number) return "The number is higer"
-            return "Correct you won"
-        }
-        if(triesNumber === 2) {
-            if(target === number) return "Correct you won"
-            return "You lose"
-        }
+        if(target === number) return "Correct you won"
+        if(triesNumber < 2) return handleIncorrectIntermediateGuesses(number)
+        if(triesNumber >= 2) return handleIncorrectLastGuess(number)
+    }
+    function handleIncorrectIntermediateGuesses(number) {
+        triesNumber++
+        if(target < number) return "The number is lower"
+        return "The number is higer"
+    }
+    function handleIncorrectLastGuess(number) {
+        return "You lose"
     }
     return {
         guessNumber
